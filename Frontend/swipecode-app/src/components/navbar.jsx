bb85c2bf07050bf2c50eb75492de5101446a5053
Anish0999 <anishgupta0999@gmail.com>
2024-11-12 11:58:07 -0500

    Initial NavBar Completed

---
+++ Frontend/swipecode-app/src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import './navbar.css'
+
+function NavBar() {
+    return (
+        <div className='navbar-div'>
+
+            <div className='title-div'>
+                <h1>SwipeCode 🚀</h1>
+            </div>  
+           
+            <div>
+                <h1>Create an Account 👤</h1>
+            </div>
+
+            <div>
+                <h1>Log in →</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default NavBar
